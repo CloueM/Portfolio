bcd758914f17486f733ac81fa8c2e870607b2004
CloueM <97562516+CloueM@users.noreply.github.com>
2026-04-04 16:16:41 -0700

    Add: about page hero and add the cat-in-mug img for page end section

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Hero from './components/Hero'
 import Projects from './components/Projects'
+import About from './components/About'
 import './App.css'
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
     <Routes>
       <Route path="/" element={<Hero />} />
       <Route path="/projects" element={<Projects />} />
+      <Route path="/about" element={<About />} />
     </Routes>
 
   )
