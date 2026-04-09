9f0d1208665e80a92bbdebc35096c62a1b2a7e7c
Cloue <senp41.mac@gmail.com>
2026-04-08 19:21:47 -0700

    Update: projects.js update the details and fix the projectitem.css to make the tab-content fits

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,104 +1,97 @@
 export const projectsData = [
   {
     id: 1,
-    title: 'Matcha Moji',
-    image: 'src/assets/images/matchamoji.png',
-    liveLink: 'https://clouemac.com/matchamoji/',
-    githubLink: 'https://github.com/CloueM/MatchaMoji-Memory-Game.git',
-    stacks: ['HTML', 'SASS', 'JavaScript'],
-    description: 'A matcha recipe discovery app with dynamic emoji-based navigation and smooth filtering.',
-    reflection: 'Building Matcha Moji helped me explore advanced frontend state management and creative filtering logic...',
-    isFeatured: true
-  },
-  {
-    id: 2,
-    title: 'LUMIX',
-    image: 'src/assets/images/lumix.png',
-    liveLink: 'https://clouemac.com/lumix/',
-    githubLink: 'https://github.com/CloueM/Lumix',
-    stacks: ['React', 'CSS', 'JavaScript', 'Figma', 'Github', 'Vite', 'Canva'],
-    description: 'A movie website where people can save their favorite movies and watch them in the future.',
-    reflection: 'This project taught me a lot about integrating third-party APIs and managing complex global state for favorites.',
-    isFeatured: true
-  },
-  {
-    id: 3,
-    title: 'APX AUTO WORKSHOP (Demo)',
-    image: 'src/assets/images/apxautoworkshop.png',
-    liveLink: 'https://apexautoworks.myshopify.com/',
-    stacks: ['Shopify'],
-    description: 'A professional booking platform and landing page for an auto repair and modification workshop.',
-    reflection: 'Focusing on accessibility and high conversion rates taught me the importance of clear UX writing and solid UI foundations.',
-    isFeatured: true
-  },
-  {
-    id: 4,
-    title: 'Cloudy Toy Co',
-    image: 'src/assets/images/cloudytoy.png',
-    liveLink: 'https://cloudytoy.bcitwebdeveloper.ca/',
-    stacks: ['Wordpress', 'WooCommerce', 'Trello', 'CSS', 'Figma'],
-    description: 'A real-time collaborative task manager for remote teams.',
-    reflection: 'Implementing real-time sockets and concurrent conflict resolution was the major challenge here.',
-    isFeatured: false
-  },
-
-  {
-    id: 5,
-    title: 'Discover Philippines (Demo)',
-    image: 'src/assets/images/discoverphilippines.png',
-    liveLink: 'https://clouemac.com/discoverphilippines/index.html',
-    githubLink: 'https://github.com/CloueM/Discover-Philippines',
-    stacks: ['HTML', 'CSS', 'JavaScript', 'Adobe Illustrator', 'Adobe XD'],
-    description: 'A real-time collaborative task manager for remote teams.',
-    reflection: 'Implementing real-time sockets and concurrent conflict resolution was the major challenge here.',
-    isFeatured: false
-  },
-
-  {
-    id: 6,
-    title: 'School Site (Demo)',
-    image: 'src/assets/images/schoolsite.png',
-    liveLink: 'https://clouemac.com/school-site/',
-    githubLink: 'https://github.com/CloueM/School-Site-Theme',
-    stacks: ['Wordpress', 'CSS', 'PHP', 'Trello'],
-    description: 'A real-time collaborative task manager for remote teams.',
-    reflection: 'Implementing real-time sockets and concurrent conflict resolution was the major challenge here.',
-    isFeatured: false
-  },
-
-  {
-    id: 7,
-    title: 'Mindset (Demo)',
-    image: 'src/assets/images/mindset.png',
-    liveLink: 'https://clouemac.com/Mindset/index.html',
-    stacks: ['Wordpress', 'CSS', 'PHP'],
-    description: 'A real-time collaborative task manager for remote teams.',
-    reflection: 'Implementing real-time sockets and concurrent conflict resolution was the major challenge here.',
-    isFeatured: false
-  },
-
-  {
-    id: 8,
     title: 'Portfolio',
     image: 'src/assets/images/portfolio.png',
     liveLink: 'https://clouemac.com/',
     githubLink: 'https://github.com/CloueM/Portfolio',
     stacks: ['React', 'CSS', 'Github', 'Vite', 'Figma', 'Canva'],
-    description: 'A real-time collaborative task manager for remote teams.',
-    reflection: 'Implementing real-time sockets and concurrent conflict resolution was the major challenge here.',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam egestas dui a leo maximus interdum. Donec venenatis, felis eu volutpat porta, dolor metus condimentum dolor, vel sodales leo urna vitae lectus. Fusce imperdiet pharetra magna, quis hendrerit nibh. Cras elementum malesuada nibh, in commodo massa bibendum id. Etiam tincidunt euismod eros eu suscipit. Etiam orci ante, dapibus quis lacus hendrerit, ultrices sagittis arcu. Proin in ligula massa. Proin efficitur iaculis rutrum. Maecenas aliquet justo ut lorem lacinia posuere. Nunc consequat a augue nec aliquet.',
+    reflection: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam egestas dui a leo maximus interdum. Donec venenatis, felis eu volutpat porta, dolor metus condimentum dolor, vel sodales leo urna vitae lectus. Fusce imperdiet pharetra magna, quis hendrerit nibh. Cras elementum malesuada nibh, in commodo massa bibendum id. Etiam tincidunt euismod eros eu suscipit. Etiam orci ante, dapibus quis lacus hendrerit, ultrices sagittis arcu. Proin in ligula massa. Proin efficitur iaculis rutrum. Maecenas aliquet justo ut lorem lacinia posuere. Nunc consequat a augue nec aliquet.',
     isFeatured: true
   },
-
   {
-    id: 9,
+    id: 2,
+    title: 'Cloudy Toy Co',
+    image: 'src/assets/images/cloudytoy.png',
+    liveLink: 'https://cloudytoy.bcitwebdeveloper.ca/',
+    stacks: ['Wordpress', 'WooCommerce', 'Trello', 'CSS', 'Figma'],
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam egestas dui a leo maximus interdum. Donec venenatis, felis eu volutpat porta, dolor metus condimentum dolor, vel sodales leo urna vitae lectus. Fusce imperdiet pharetra magna, quis hendrerit nibh. Cras elementum malesuada nibh, in commodo massa bibendum id. Etiam tincidunt euismod eros eu suscipit. Etiam orci ante, dapibus quis lacus hendrerit, ultrices sagittis arcu. Proin in ligula massa. Proin efficitur iaculis rutrum. Maecenas aliquet justo ut lorem lacinia posuere. Nunc consequat a augue nec aliquet.',
+    reflection: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam egestas dui a leo maximus interdum. Donec venenatis, felis eu volutpat porta, dolor metus condimentum dolor, vel sodales leo urna vitae lectus. Fusce imperdiet pharetra magna, quis hendrerit nibh. Cras elementum malesuada nibh, in commodo massa bibendum id. Etiam tincidunt euismod eros eu suscipit. Etiam orci ante, dapibus quis lacus hendrerit, ultrices sagittis arcu. Proin in ligula massa. Proin efficitur iaculis rutrum. Maecenas aliquet justo ut lorem lacinia posuere. Nunc consequat a augue nec aliquet.',
+    isFeatured: false
+  },
+  {
+    id: 3,
+    title: 'School Site (Demo)',
+    image: 'src/assets/images/schoolsite.png',
+    liveLink: 'https://clouemac.com/school-site/',
+    githubLink: 'https://github.com/CloueM/School-Site-Theme',
+    stacks: ['Wordpress', 'CSS', 'PHP', 'Trello'],
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam egestas dui a leo maximus interdum. Donec venenatis, felis eu volutpat porta, dolor metus condimentum dolor, vel sodales leo urna vitae lectus. Fusce imperdiet pharetra magna, quis hendrerit nibh. Cras elementum malesuada nibh, in commodo massa bibendum id. Etiam tincidunt euismod eros eu suscipit. Etiam orci ante, dapibus quis lacus hendrerit, ultrices sagittis arcu. Proin in ligula massa. Proin efficitur iaculis rutrum. Maecenas aliquet justo ut lorem lacinia posuere. Nunc consequat a augue nec aliquet.',
+    reflection: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam egestas dui a leo maximus interdum. Donec venenatis, felis eu volutpat porta, dolor metus condimentum dolor, vel sodales leo urna vitae lectus. Fusce imperdiet pharetra magna, quis hendrerit nibh. Cras elementum malesuada nibh, in commodo massa bibendum id. Etiam tincidunt euismod eros eu suscipit. Etiam orci ante, dapibus quis lacus hendrerit, ultrices sagittis arcu. Proin in ligula massa. Proin efficitur iaculis rutrum. Maecenas aliquet justo ut lorem lacinia posuere. Nunc consequat a augue nec aliquet.',
+    isFeatured: false
+  },
+  {
+    id: 4,
+    title: 'LUMIX',
+    image: 'src/assets/images/lumix.png',
+    liveLink: 'https://clouemac.com/lumix/',
+    githubLink: 'https://github.com/CloueM/Lumix',
+    stacks: ['React', 'CSS', 'JavaScript', 'Adobe Photoshop', 'Github', 'Vite', 'Canva'],
+    description: 'LUMIX is a fully functional movie discovery application built entirely with React. It allows users to browse through different categories like Now Playing, Popular, Top Rated, and Upcoming, while viewing detailed movie information and watching trailers. To make the experience more personalized, users can also save their favorite movies to their own list so they can easily find and watch them later.',
+    reflection: 'Building this project was a major stepping stone in my front-end journey, particularly in learning how to integrate third-party APIs and manage complex global states. I really enjoyed the challenge of fetching movie data from the TMDB API and displaying it seamlessly on the front end. While I grasped the core concepts, working hands-on with APIs showed me areas where I want to gain even more practice. When I ran into roadblocks, I learned how to effectively search for solutions using developer communities and AI, which is a crucial problem-solving skill.\n\nOne of my biggest challenges came from being highly ambitious during the design phase. I created beautiful, high-fidelity wireframes in Photoshop, but bringing those exact designs to life in code proved challenging. For instance, making the category navigation bar fully responsive across all device sizes required me to rethink my initial design mid-development. This taught me a valuable lesson in balancing design aesthetics with technical functionality.\n\nUltimately, this project highlighted the importance of a solid development plan. I learned that things do not always go exactly as wireframed, and being able to adapt, pivot, and implement solutions on the fly is just as critical as writing good code. It showed me how design and development must work closely together to create a great user experience.',
+    isFeatured: true
+  },
+  {
+    id: 5,
+    title: 'APX AUTO WORKSHOP (Demo)',
+    image: 'src/assets/images/apxautoworkshop.png',
+    liveLink: 'https://apexautoworks.myshopify.com/',
+    stacks: ['Shopify'],
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam egestas dui a leo maximus interdum. Donec venenatis, felis eu volutpat porta, dolor metus condimentum dolor, vel sodales leo urna vitae lectus. Fusce imperdiet pharetra magna, quis hendrerit nibh. Cras elementum malesuada nibh, in commodo massa bibendum id. Etiam tincidunt euismod eros eu suscipit. Etiam orci ante, dapibus quis lacus hendrerit, ultrices sagittis arcu. Proin in ligula massa. Proin efficitur iaculis rutrum. Maecenas aliquet justo ut lorem lacinia posuere. Nunc consequat a augue nec aliquet.',
+    reflection: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam egestas dui a leo maximus interdum. Donec venenatis, felis eu volutpat porta, dolor metus condimentum dolor, vel sodales leo urna vitae lectus. Fusce imperdiet pharetra magna, quis hendrerit nibh. Cras elementum malesuada nibh, in commodo massa bibendum id. Etiam tincidunt euismod eros eu suscipit. Etiam orci ante, dapibus quis lacus hendrerit, ultrices sagittis arcu. Proin in ligula massa. Proin efficitur iaculis rutrum. Maecenas aliquet justo ut lorem lacinia posuere. Nunc consequat a augue nec aliquet.',
+    isFeatured: true
+  },
+  {
+    id: 6,
+    title: 'Mindset (Demo)',
+    image: 'src/assets/images/mindset.png',
+    liveLink: 'https://clouemac.com/Mindset/index.html',
+    stacks: ['Wordpress', 'CSS', 'PHP'],
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam egestas dui a leo maximus interdum. Donec venenatis, felis eu volutpat porta, dolor metus condimentum dolor, vel sodales leo urna vitae lectus. Fusce imperdiet pharetra magna, quis hendrerit nibh. Cras elementum malesuada nibh, in commodo massa bibendum id. Etiam tincidunt euismod eros eu suscipit. Etiam orci ante, dapibus quis lacus hendrerit, ultrices sagittis arcu. Proin in ligula massa. Proin efficitur iaculis rutrum. Maecenas aliquet justo ut lorem lacinia posuere. Nunc consequat a augue nec aliquet.',
+    reflection: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam egestas dui a leo maximus interdum. Donec venenatis, felis eu volutpat porta, dolor metus condimentum dolor, vel sodales leo urna vitae lectus. Fusce imperdiet pharetra magna, quis hendrerit nibh. Cras elementum malesuada nibh, in commodo massa bibendum id. Etiam tincidunt euismod eros eu suscipit. Etiam orci ante, dapibus quis lacus hendrerit, ultrices sagittis arcu. Proin in ligula massa. Proin efficitur iaculis rutrum. Maecenas aliquet justo ut lorem lacinia posuere. Nunc consequat a augue nec aliquet.',
+    isFeatured: false
+  },
+  {
+    id: 7,
+    title: 'Matcha Moji',
+    image: 'src/assets/images/matchamoji.png',
+    liveLink: 'https://clouemac.com/matchamoji/',
+    githubLink: 'https://github.com/CloueM/MatchaMoji-Memory-Game.git',
+    stacks: ['HTML', 'SASS', 'JavaScript'],
+    description: 'Matcha Moji is a browser-based memory matching game built entirely with vanilla JavaScript. Players race against the clock to match pairs of emoji cards, selecting from four difficulty levels: Easy, Medium, Hard, and "WTH". I engineered a custom scoring system that saves directly to the user\'s local storage. This system calculates the final score based on a weighted combination of player accuracy (60%), remaining time (40%), and a multiplier tied to the chosen difficulty level.',
+    reflection: 'While I was very comfortable with the HTML and CSS aspects of this project, diving deep into JavaScript logic was a great learning experience. Understanding the concepts was straightforward, but structuring the logic and applying the correct syntax efficiently took some trial and error. Building this game made me realize how much I enjoy solving complex logic puzzles, and it inspired me to take on more interactive projects to continue refining my JavaScript skills.\n\nAnother major takeaway from Matcha Moji was the importance of the "Mobile First" approach. When I started styling the game, I focused entirely on creating a great desktop layout. Once it was finished, I realized I hadn\'t accounted for mobile users. Retrofitting the design to work seamlessly on smaller screens was a frustrating struggle that could have been easily avoided with better initial planning.\n\nMoving forward, I now integrate mobile responsiveness from the very beginning of my design and development process. This project was a practical lesson in thinking about the end-user experience across all devices before writing a single line of CSS.',
+    isFeatured: false
+  },
+  {
+    id: 8,
     title: 'Alto Pharmacy (Demo)',
     image: 'src/assets/images/altopharmacy.png',
     githubLink: 'https://github.com/CloueM/G4-Project-2',
-    stacks: ['HTML', 'CSS', 'JavaScript', 'Github'],
-    description: 'A real-time collaborative task manager for remote teams.',
-    reflection: 'Implementing real-time sockets and concurrent conflict resolution was the major challenge here.',
+    stacks: ['HTML', 'CSS', 'JavaScript', 'Github', 'Trello'],
+    description: 'For this collaborative team project, we tasked ourselves with recreating the front-page design of the real-world Alto Pharmacy website. Using HTML, CSS, and JavaScript, our goal was to accurately translate an existing professional design into clean, functional code while working together smoothly as a front-end development team.',
+    reflection: 'This project significantly sharpened my CSS skills, but the most valuable lessons came from the project management and collaboration side. Working with a team introduced us to the complexities of version control using GitHub. Early on, we faced the common headache of merge conflicts. Since we were all beginners but I had a bit of prior exposure, I took the initiative to guide the team through GitHub basics so we could collaborate without overwriting each other\'s work.\n\nTo keep us organized and on track, I also introduced the team to Trello for task management. This was my first time seriously using project management tools for a group effort, and it proved essential. By mapping out our workflow, we were able to clearly assign tasks and divide the workload in a way that minimized overlapping code and reduced future conflicts.\n\nOverall, this experience went far beyond just writing code. It taught me how to communicate effectively with other developers, manage a project workflow, and step up to lead problem-solving efforts when technical roadblocks arise—skills that are essential for any successful team.',
     isFeatured: true
+  },
+  {
+    id: 9,
+    title: 'Discover Philippines (Demo)',
+    image: 'src/assets/images/discoverphilippines.png',
+    liveLink: 'https://clouemac.com/discoverphilippines/index.html',
+    githubLink: 'https://github.com/CloueM/Discover-Philippines',
+    stacks: ['HTML', 'CSS', 'JavaScript', 'Adobe Illustrator', 'Figma'],
+    description: 'Discover Philippines is an informational website built entirely with hardcoded HTML and CSS. Created as a class project to showcase a specific country, the site guides users through different aspects of the Philippines, featuring dedicated pages for Home, Locations, Culture, and Foods. The project involved end-to-end creation, from initial content planning and custom logo design to wireframing and final front-end development.',
+    reflection: 'Building a multi-page website from scratch using pure HTML and CSS taught me the true value of patience and attention to detail. Every piece of content, image, and style had to be manually integrated across multiple pages. To make the process smoother, I focused on learning how to write reusable CSS styles, which drastically improved my workflow and kept the visual design consistent throughout the entire site.\n\nOne of my main technical challenges was moving away from using standard `<div>` tags for every section. I pushed myself to learn and implement proper semantic HTML tags, which improved the accessibility and professional structure of my code. Organizing the gathered information and ensuring there were no visual errors or broken layouts across the different pages required a high level of organization.\n\nDespite the manual effort required for a completely hardcoded site, I really enjoyed the creative challenge of designing each page individually. It gave me a strong foundational understanding of web structure and styling, which has been incredibly useful as I’ve transitioned into using more modern frameworks and CMS platforms.',
+    isFeatured: false
   }
-
-
 ];
